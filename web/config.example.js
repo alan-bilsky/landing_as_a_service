ee8278409d98461cb2893fcc612e879554236ada
alan-bilsky <alan.bilsky@teracloud.io>
2025-06-10 15:21:15 -0300

    asd

--- web/config.example.js
+++
@@ -1,8 +0,0 @@
-// Rename this file to config.js and fill in the values from `terragrunt output`.
-const config = {
-    region: 'us-west-2',
-    userPoolId: '<cognito-user-pool-id>',
-    userPoolClientId: '<cognito-user-pool-client-id>',
-    apiEndpoint: '<api-gateway-endpoint>',
-    cloudfrontUrl: '<cloudfront-distribution-url>'
-};
